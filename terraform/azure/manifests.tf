45bf57367254a960c861713ad7454b07619f027c
Evgeniy Dmitriev <edmitriev@wallarm.com>
2022-12-15 00:38:11 +0700

    Fix dependencies for kubernetes_manifest resource

--- terraform/azure/manifests.tf
+++ terraform/azure/manifests.tf
@@ -1,5 +1,6 @@
 resource "kubernetes_manifest" "main" {
   depends_on = [
+    azurerm_kubernetes_cluster.aks,
     helm_release.external_ingress_nginx,
     helm_release.internal_ingress_wallarm
   ]
